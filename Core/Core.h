64f1474ec828cde1744b7acc7643c71e511ebf77
Alexander Koshkej <61204471+Koshkej@users.noreply.github.com>
2026-01-07 02:15:39 +0300

    Create Core.h

---
+++ Core/Core.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <random>
+#include <cmath>
+
+#include <SFML/Graphics.hpp> 
+
+#define VERSION "0.1"
+#define VIDEOMODE {1000, 500}
+#define TITLE "NN Workspace"
+#define EULER 2.7182818284
+
+extern sf::RenderWindow generalWindow;
+
+std::mt19937& getGenerator();
+void sigmoid(double& ele);
+void sigmoidDerivative(double& ele);
