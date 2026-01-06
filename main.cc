894e1cc0618029c6df39580bcca94f4dc732fdb0
Alexander Koshkej <61204471+Koshkej@users.noreply.github.com>
2026-01-07 02:16:50 +0300

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,11 @@
+#include "Core/Core.h"
+#include "Matrix/Matrix.h"
+#include "NN/NN.h"
+#include "Dataset/Dataset.h"
+
+NN::SimplePerceptron numberDeterminingPerceptron(784, 100, 10, 0.3); // Example
+
+int main()
+{
+  return 0;
+}
