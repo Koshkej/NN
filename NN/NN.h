b7456b5ef49cbe8d214e0d4e8fcae7e4e54ceb2e
Alexander Koshkej <61204471+Koshkej@users.noreply.github.com>
2026-01-08 14:24:25 +0300

    Update NN.h

--- NN/NN.h
+++ NN/NN.h
@@ -8,12 +8,12 @@ namespace NN {
 
 	class SimplePerceptron {
 	public:
-		SimplePerceptron(int inputLayerNumber, int hiddenLayerNumber, int outputLayerNumber, double learningRate);
+		SimplePerceptron(size_t inputLayerNumber, size_t hiddenLayerNumber, size_t outputLayerNumber, double learningRate);
 		std::vector<double> query(const std::vector<double>& input);
 		void train(const std::vector<double>& input, const std::vector<double>& target);
 	private:
 		Matrix weightsHidden_Input, weightsOutput_Hidden;
-		int inputLayerNumber, hiddenLayerNumber, outputLayerNumber;
+		size_t inputLayerNumber, hiddenLayerNumber, outputLayerNumber;
 		double learningRate;
 	};
 
