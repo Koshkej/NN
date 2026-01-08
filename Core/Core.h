accfef9f58f84e6410dd4eefa1902d1ddd94a2ca
Alexander Koshkej <61204471+Koshkej@users.noreply.github.com>
2026-01-08 14:25:19 +0300

    Update Core.h

--- Core/Core.h
+++ Core/Core.h
@@ -5,6 +5,11 @@
 #include <random>
 #include <cmath>
 
+#define DEBUG // !
+
+#define VERSION "0.1"
+#define VIDEOMODE {1000, 500}
+#define TITLE "NN Workspace"
 #define EULER 2.7182818284
 
 extern sf::RenderWindow generalWindow;
